bdfa5fa4e5d03a70eef752c81f172ba6dd51d0da
zablockiy <105744371+zablockiy@users.noreply.github.com>
2022-12-14 08:46:39 +0200

    upd Profile.js

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -4,31 +4,31 @@ import style from './profile.module.css';
 
 
 
-export const Profile = (props)=> {
+export const Profile = ({username, avatar, tag, location, stats})=> {
  return  (<div className={style.profile}>
     <div className={style.description}>
       <img
-        src={props.avatar}
+        src={avatar}
         alt="User avatar"
         className={style.avatar}
       />
-      <p className={style.name}>{props.username}</p>
-      <p className={style.tag}>@{props.tag}</p>
-      <p className={style.location}>{props.location}</p>
+      <p className={style.name}>{username}</p>
+      <p className={style.tag}>@{tag}</p>
+      <p className={style.location}>{location}</p>
     </div>
 
     <ul className={style.stats}>
       <li>
         <span className={style.label}>Followers</span>
-        <span className={style.quantity}>{props.stats.followers}</span>
+        <span className={style.quantity}>{stats.followers}</span>
       </li>
       <li>
         <span className={style.label}>Views</span>
-        <span className={style.quantity}>{props.stats.views}</span>
+        <span className={style.quantity}>{stats.views}</span>
       </li>
       <li>
         <span className={style.label}>Likes</span>
-        <span className={style.quantity}>{props.stats.likes}</span>
+        <span className={style.quantity}>{stats.likes}</span>
       </li>
     </ul>
   </div>);
